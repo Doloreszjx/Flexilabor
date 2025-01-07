064d0ec34b612a0ed84168b2904af7e6d8dae1fa
doloreszjx <1173827505@qq.com>
2025-01-07 16:14:21 +0800

    add: mobile plan

--- code/web-backend/config/mongoDb.js
+++ code/web-backend/config/mongoDb.js
@@ -1,14 +1,33 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const User = require('../models/userModel');
+const Transactions = require('../models/transactionsModel');
+const Jobs = require('../models/jobsModel');
+
 const connectDatabase = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Database is connected");
-  } catch (error) {
-    console.error("Database Connection failed with an error:", error.message);
-    process.exit(1);
-  }
+	try {
+		await mongoose.connect(process.env.MONGO_URI);
+		console.log('Database is connected');
+		// // 假设存在名为 User 的模型，查询所有用户数据并输出
+		// try {
+		// 	const users = await User.find({});
+		// 	console.log('Users:', users);
+
+		// 	// 假设存在名为 Job 的模型，查询所有职位数据并输出
+		// 	const jobs = await Jobs.find({});
+		// 	console.log('Jobs:', jobs);
+
+		// 	// 假设存在名为 Transaction 的模型，查询所有交易数据并输出
+		// 	const transactions = await Transactions.find({});
+		// 	console.log('Transactions:', transactions);
+		// } catch (error) {
+		// 	console.log('error');
+		// }
+	} catch (error) {
+		console.error('Database Connection failed with an error:', error.message);
+		process.exit(1);
+	}
 };
 
 module.exports = connectDatabase;
--- code/web-backend/models/userModel.js
+++ code/web-backend/models/userModel.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  uid: { type: String, required: true, unique: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  abn: { type: String, required: false }, 
-  role: { type: String, required: true,  enum: ['Customer', 'Contractor', 'Worker'] },
-  workType: { type: String, required: false },
-  profilePicture: { type: String, required: false },
-  tools: {type: String, required: false},
-  contacts: { type: [String], default: [] }
+	uid: { type: String, required: true, unique: true },
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
+	email: { type: String, required: true, unique: true },
+	abn: { type: String, required: false },
+	role: {
+		type: String,
+		required: true,
+		enum: ['Customer', 'Contractor', 'Worker'],
+	},
+	workType: { type: String, required: false },
+	profilePicture: { type: String, required: false },
+	tools: { type: String, required: false },
+	contacts: { type: [String], default: [] },
 });
 
 module.exports = mongoose.model('User', userSchema);
